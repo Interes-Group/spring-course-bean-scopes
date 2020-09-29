9ab06c2490d12a6895a89c95b479bd1c91f35035
Milan Mladoniczky <6153201+mladoniczky@users.noreply.github.com>
2020-09-29 18:13:20 +0200

    Init commit

---
+++ src/main/java/sk/stuba/fei/uim/asos/cvicenie1/beanscopes/BeanScopesApplication.java
@@ -0,0 +1,48 @@
+package sk.stuba.fei.uim.asos.cvicenie1.beanscopes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BeanScopesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanScopesApplication.class, args);
+    }
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @GetMapping("/")
+    public String getNames() throws InterruptedException {
+        SingletonBean singletonBean = appContext.getBean(SingletonBean.class);
+        PrototypeBean prototypeBean = appContext.getBean(PrototypeBean.class);
+        RequestBean requestBean = appContext.getBean(RequestBean.class);
+        SessionBean sessionBean = appContext.getBean(SessionBean.class);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(singletonBean.getName()).append("\n");
+        builder.append(prototypeBean.getName()).append("\n");
+        builder.append(requestBean.getName()).append("\n");
+        builder.append(sessionBean.getName()).append("\n");
+
+        Thread.sleep(500);
+        builder.append("\n");
+
+        singletonBean = appContext.getBean(SingletonBean.class);
+        prototypeBean = appContext.getBean(PrototypeBean.class);
+        requestBean = appContext.getBean(RequestBean.class);
+
+        builder.append(singletonBean.getName()).append("\n");
+        builder.append(prototypeBean.getName()).append("\n");
+        builder.append(requestBean.getName()).append("\n");
+        builder.append(sessionBean.getName()).append("\n");
+
+        return builder.toString();
+    }
+}
