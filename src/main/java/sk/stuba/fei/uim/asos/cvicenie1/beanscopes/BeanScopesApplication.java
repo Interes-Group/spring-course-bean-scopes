87b659c8da839f1c9db9b9854c608e7b24d87b64
Juraj Mažári <juraj.mazari@stuba.sk>
2020-10-05 14:45:05 +0200

    refactor using Spring Framework

--- src/main/java/sk/stuba/fei/uim/asos/cvicenie1/beanscopes/BeanScopesApplication.java
+++ src/main/java/sk/stuba/fei/uim/asos/cvicenie1/beanscopes/BeanScopesApplication.java
@@ -1,51 +1,58 @@
 package sk.stuba.fei.uim.asos.cvicenie1.beanscopes;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@RestController
-@SpringBootApplication
+import java.util.logging.Logger;
+
+//@RestController
 public class BeanScopesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeanScopesApplication.class, args);
-    }
+    private static final Logger log = Logger.getLogger(BeanScopesApplication.class.getSimpleName());
 
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext appContext = new ClassPathXmlApplicationContext(new String[]{"config.xml"});
 
-    private final ApplicationContext appContext;
-
-    public BeanScopesApplication(ApplicationContext appContext) {
-        this.appContext = appContext;
-    }
-
-    @GetMapping("/")
-    public String getNames() throws InterruptedException {
-        SingletonBean singletonBean = appContext.getBean(SingletonBean.class);
-        PrototypeBean prototypeBean = appContext.getBean(PrototypeBean.class);
-        RequestBean requestBean = appContext.getBean(RequestBean.class);
-        SessionBean sessionBean = appContext.getBean(SessionBean.class);
-
-        StringBuilder builder = new StringBuilder();
-        builder.append(singletonBean.getName()).append("\n");
-        builder.append(prototypeBean.getName()).append("\n");
-        builder.append(requestBean.getName()).append("\n");
-        builder.append(sessionBean.getName()).append("\n");
+        logBeanNames(appContext);
 
         Thread.sleep(500);
-        builder.append("\n----- After 500ms -----\n\n");
 
-        singletonBean = appContext.getBean(SingletonBean.class);
-        prototypeBean = appContext.getBean(PrototypeBean.class);
-        requestBean = appContext.getBean(RequestBean.class);
-
-        builder.append(singletonBean.getName()).append("\n");
-        builder.append(prototypeBean.getName()).append("\n");
-        builder.append(requestBean.getName()).append("\n");
-        builder.append(sessionBean.getName()).append("\n");
-
-        return builder.toString();
+        logBeanNames(appContext);
     }
+
+    private static void logBeanNames(ApplicationContext appContext) {
+        SingletonBean singletonBean = appContext.getBean(SingletonBean.class);
+        PrototypeBean prototypeBean = appContext.getBean(PrototypeBean.class);
+
+        log.info(singletonBean.getName());
+        log.info(prototypeBean.getName());
+    }
+//
+//    @GetMapping("/")
+//    public String getNames() throws InterruptedException {
+//        SingletonBean singletonBean = appContext.getBean(SingletonBean.class);
+//        PrototypeBean prototypeBean = appContext.getBean(PrototypeBean.class);
+//        RequestBean requestBean = appContext.getBean(RequestBean.class);
+//        SessionBean sessionBean = appContext.getBean(SessionBean.class);
+//
+//        StringBuilder builder = new StringBuilder();
+//        builder.append(singletonBean.getName()).append("\n");
+//        builder.append(prototypeBean.getName()).append("\n");
+//        builder.append(requestBean.getName()).append("\n");
+//        builder.append(sessionBean.getName()).append("\n");
+//
+//        Thread.sleep(500);
+//        builder.append("\n----- After 500ms -----\n\n");
+//
+//        singletonBean = appContext.getBean(SingletonBean.class);
+//        prototypeBean = appContext.getBean(PrototypeBean.class);
+//        requestBean = appContext.getBean(RequestBean.class);
+//
+//        builder.append(singletonBean.getName()).append("\n");
+//        builder.append(prototypeBean.getName()).append("\n");
+//        builder.append(requestBean.getName()).append("\n");
+//        builder.append(sessionBean.getName()).append("\n");
+//
+//        return builder.toString();
+//    }
 }
